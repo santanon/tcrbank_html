ef958f08c6b7d77a37261ba948be25164a2faba8
santanon <47654173+santanon@users.noreply.github.com>
2021-05-10 11:14:11 +0700

    Update landingpage.blade.php

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Landing Page : ไทยเครดิตsdasdasdasd</title>
+    <title>Landing Page : ไทยเครดิต</title>
 
     <meta name="keywords" content="" />
     <meta name="description" content="" />
